0dac9358cff401719dcd56083ca2d75f51ce58dc
HyeonJi <jhg050908@naver.com>
2023-01-16 12:08:07 +0900

    # Refactor : Alarm Inside Fragment xml파일 수정 # # 버튼 클릭시 테두리 효과

--- sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/AlarmFragment.kt
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/AlarmFragment.kt
@@ -5,14 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import com.example.sleeper_frontend.databinding.FragmentAlarmBinding
 
 class AlarmFragment : Fragment(R.layout.fragment_alarm) {
 
+    private lateinit var binding : FragmentAlarmBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentAlarmBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false)
+        binding.btnUpdate.setOnClickListener {
+            val alarmInsideFragment = AlarmInsideFragment()
+            val transaction : FragmentTransaction = requireFragmentManager().beginTransaction()
+            transaction.replace(R.id.fl_container, alarmInsideFragment).commit()
+        }
+        return binding.root
     }
 }
