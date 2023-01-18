6f057ecd268272738834ba2985a7517e40bc0585
HyeonJi <jhg050908@naver.com>
2023-01-18 22:10:39 +0900

    # Refactor : LoginResponse 변수 추가 # # userPk 추가

--- sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/dto/login/LoginResponse.kt
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/dto/login/LoginResponse.kt
@@ -3,5 +3,6 @@ package com.example.sleeper_frontend.dto.login
 data class LoginResponse(
     var message : String,
     var accessToken : String,
-    var refreshToken : String
+    var refreshToken : String,
+    var userPk : Long
 )
