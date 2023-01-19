07ecfebcda5b739ccbad55ff4e1095f9e6217590
HyeonJi <jhg050908@naver.com>
2023-01-19 23:30:19 +0900

    # Update : Calendar Fragment 기능 수정 # # Calender Inside Fragment에 날짜 넘겨줌

--- sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/CalendarFragment.kt
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/CalendarFragment.kt
@@ -1,10 +1,12 @@
 package com.example.sleeper_frontend
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.sleeper_frontend.databinding.FragmentCalendarBinding
@@ -24,20 +26,49 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar) {
 
         binding =  FragmentCalendarBinding.inflate(inflater, container, false)
 
-        val todayDecorator = context?.let { TodayDecorator(it) }
-        binding.calendar.addDecorators(todayDecorator)
+        //다시 돌아왔을 때, selectedday에만 표시/
+
+
+//        val todayDecorator = context?.let { TodayDecorator(it) }
+//        binding.calendar.addDecorators(todayDecorator)
+
+        lateinit var selectedDate : CalendarDay
+
+        if(binding.calendar.selectedDate != null) {
+            selectedDate = binding.calendar.selectedDate
+
+            val eventDecorator = context?.let { EventDecorator(it, selectedDate) }
+            binding.calendar.addDecorator(eventDecorator)
+        }
 
         binding.calendar.setOnDateChangedListener { widget, date, selected ->
 
-            binding.calendar.removeDecorator(todayDecorator)
+//            binding.calendar.removeDecorator(todayDecorator)
 
-            val selectedDate: CalendarDay = binding.calendar.selectedDate
-            // eventDecorator는 선택된 날짜를 커스텀해주는 decorator
+            selectedDate = binding.calendar.selectedDate
 
             val eventDecorator = context?.let { EventDecorator(it, selectedDate) }
             binding.calendar.addDecorator(eventDecorator)
 
+            val bundle : Bundle = Bundle()
+            val data = binding.calendar.selectedDate
+            val year = data.year.toString()
+            val temp = data.month + 1
+            var month = temp.toString()
+
+            if(month.length < 2) {
+                month = "0${month}"
+            }
+
+            val day = data.day.toString()
+
+
+            bundle.putString("year", year)
+            bundle.putString("month", month)
+            bundle.putString("day",day)
+
             val calendarInsideFragment = CalendarInsideFragment()
+            calendarInsideFragment.arguments = bundle
             val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
             transaction.replace(R.id.fl_container, calendarInsideFragment).addToBackStack("CalendarFragment").commit()
         }
